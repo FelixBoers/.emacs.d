73b8344ebce95db1b86e8b93dee64a1beb8a5552
Felix Boers <f.boers@controlexpert.com>
2016-11-02 15:12:38 +0100

    change agenda file location

--- init.el
+++ init.el
@@ -78,4 +78,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/.emacs.d/init.org"))))
+ '(org-agenda-files '("~/org"))
+ )
